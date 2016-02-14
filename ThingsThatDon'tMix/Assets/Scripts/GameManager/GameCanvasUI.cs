b5244ab233df2cbb21d07f7d481f158d116009a2
Rory O'Connor <r.angus.oconnor@gmail.com>
2016-02-14 06:34:31 -0500

    game over title and score

---
+++ ThingsThatDon'tMix/Assets/Scripts/GameManager/GameCanvasUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameCanvasUI : MonoBehaviour {
+
+    public Text Score;
+
+	void Update ()
+    {
+        Score.text = GameManager.Instance.score.ToString();
+	}
+}
